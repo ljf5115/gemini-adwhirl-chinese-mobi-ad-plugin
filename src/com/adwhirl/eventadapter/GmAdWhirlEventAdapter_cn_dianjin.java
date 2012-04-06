5fa9e8e46b1856d1e3fec06409bdede80c6cc60e
gotonyu1979 <gotonyu1979@gmail.com>
2012-04-06 21:56:28 +0800

    上次submit的是老文件。。。 升级了小版本号

--- src/com/adwhirl/eventadapter/GmAdWhirlEventAdapter_cn_dianjin.java
+++ src/com/adwhirl/eventadapter/GmAdWhirlEventAdapter_cn_dianjin.java
@@ -1,16 +1,33 @@
 package com.adwhirl.eventadapter;
 
+import android.content.Context;
 import android.graphics.Color;
 import android.view.View;
 
 import com.adwhirl.AdWhirlLayout;
 import com.adwhirl.eventadapter.GmAdWhirlEventHandler.GmEventADType;
 import com.adwhirl.eventadapter.IGmAdWhirlEventAdapter.GmAdWhirlCustomEventAdapter;
+import com.nd.dianjin.DianJinPlatform;
 import com.nd.dianjin.OfferBanner;
 import com.nd.dianjin.utility.BannerColorConfig;
 
 public class GmAdWhirlEventAdapter_cn_dianjin extends GmAdWhirlCustomEventAdapter {
 	OfferBanner mOfferBanner = null;
+	private static boolean mInitializedFlag = false;
+	public static void initialize(Context context) {
+		if (!mInitializedFlag) {
+			int id = 0;
+			try {
+				id = Integer.parseInt(GmAdWhirlEventAdapterData
+						.getPublishID(GmEventADType.dianjin));
+			} catch (Exception e) {
+
+			}	
+			DianJinPlatform.initialize(context, id,
+					GmAdWhirlEventAdapterData.getDefaultPwdID(GmEventADType.dianjin));
+			mInitializedFlag = true;
+		}
+	}
 	public GmAdWhirlEventAdapter_cn_dianjin(AdWhirlLayout adLayout, Object obj) {
 		super(adLayout, obj);
 	}
@@ -20,12 +37,11 @@ public class GmAdWhirlEventAdapter_cn_dianjin extends GmAdWhirlCustomEventAdapte
 		gmEventAdapterLog("dianjin->init");
 		AdWhirlLayout adLayout = getAdwhirlLayout();
 		if (adLayout != null) {
-//			final RelativeLayout mainLayout = new RelativeLayout(getAdwhirlActivity());
 			BannerColorConfig colorConfig = new BannerColorConfig();
 			colorConfig.setBackgroundColor(Color.BLUE);
-			colorConfig.setDetailColor(0xFF3300);
-			colorConfig.setNameColor(0xFF3300);
-			colorConfig.setRewardColor(0xFF3300);
+			colorConfig.setDetailColor(0xFFFF3300);
+			colorConfig.setNameColor(0xFFFF3300);
+			colorConfig.setRewardColor(0xFFFF3300);
 			int id = 0;
 			try {
 				id = Integer.parseInt(GmAdWhirlEventAdapterData
@@ -33,10 +49,10 @@ public class GmAdWhirlEventAdapter_cn_dianjin extends GmAdWhirlCustomEventAdapte
 			} catch (Exception e) {
 
 			}	
+			initialize(getAdwhirlActivity());
 			mOfferBanner = new OfferBanner(getAdwhirlActivity(), id,
 					GmAdWhirlEventAdapterData.getDefaultPwdID(GmEventADType.dianjin),
 					6000, OfferBanner.AnimationType.ANIMATION_PUSHUP, colorConfig);
-//			mainLayout.addView(space, lp);
 
 			adLayout.addView(mOfferBanner);
 			// Show AD for certain seconds (extra.cycleTime).
@@ -52,7 +68,6 @@ public class GmAdWhirlEventAdapter_cn_dianjin extends GmAdWhirlCustomEventAdapte
 		if (tmpLayout != null) {
 			if (mOfferBanner != null) {
 				mOfferBanner.setVisibility(View.GONE);
-				// mADView.setListener(null);
 				tmpLayout.removeView(mOfferBanner);
 				mOfferBanner = null;
 				gmEventAdapterLog("dianjin->removed");
